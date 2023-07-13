87be837df4f4480d1477ece45c930aef3a32f983
adibmannan <adibmannan1@gmail.com>
2023-07-13 23:27:39 +0600

    product screen added

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -6,11 +6,11 @@ import Rating from './Rating'
 const Product = ({product}) => {
   return (
     <Card className='my-3 p-2 rounded card'>
-        <Link to={`/products/${product._id}`} className='img' >
+        <Link to={`/product/${product._id}`} className='img' >
             <Card.Img className='img' src={product.image} variant='top'></Card.Img>
         </Link>
         <Card.Body className='cardBody'>
-            <Link to={`/products/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Title as="div" className='product-title'>
                     <strong>{product.name}</strong>
                 </Card.Title>
