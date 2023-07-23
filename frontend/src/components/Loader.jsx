54b25c259a0e770f3a02595780de35f77029f0fa
adibmannan <adibmannan1@gmail.com>
2023-07-23 12:31:37 +0600

    custom react loader added

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,19 +1,30 @@
 import { Spinner } from "react-bootstrap";
+import HashLoader from "react-spinners/HashLoader";
 
 import React from 'react'
 
 const Loader = () => {
   return (
-    <Spinner 
-    animation="border"
-    role="status"
-    style={{
-        width: "100px",
-        height: "100px",
-        margin: "auto",
-        display: "block",
-        color: "#082750"
-    }}/>
+    // <Spinner 
+    // animation="border"
+    // role="status"
+    // style={{
+    //     width: "100px",
+    //     height: "100px",
+    //     margin: "auto",
+    //     display: "block",
+    //     color: "#082750"
+    // }}/>
+    <HashLoader
+        color="#082750"
+        size={120}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+        style={{
+            margin: "auto",
+            display: "block",
+          }}
+    />
   )
 }
 
